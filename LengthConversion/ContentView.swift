bac528a6b6c45987cd86cecd404a3891db60a334
Matilda Mared <matildamared@live.se>
2022-10-20 10:33:41 +0200

    add logic

--- LengthConversion/ContentView.swift
+++ LengthConversion/ContentView.swift
@@ -29,7 +29,74 @@ struct ContentView: View {
     
     @State private var fromUnit = "meters"
     @State private var toUnit = "feet"
-    @State private var length = 0
+    @State private var length: Double = 0
+    
+    var result: Double {
+        switch fromUnit {
+        case "meters":
+            if toUnit == "kilometers" {
+                return length / 1000
+            } else if toUnit == "feet" {
+                return length * 3.28084
+            } else if toUnit == "yards" {
+                return length * 1.0936
+            } else if toUnit == "miles" {
+                return length * 0.00062137
+            } else {
+                return length
+            }
+        case "kilometers":
+            if toUnit == "meters" {
+                return length * 1000
+            } else if toUnit == "feet" {
+                return length * 3280.84
+            } else if toUnit == "yards" {
+                return length * 1093.6133
+            } else if toUnit == "miles" {
+                return length * 0.621371192
+            } else {
+                return length
+            }
+        case "feet":
+            if toUnit == "meters" {
+                return length * 0.3048
+            } else if toUnit == "yards" {
+                return length / 3
+            } else if toUnit == "miles" {
+                return length * 0.000189393939
+            } else if toUnit == "kilometers" {
+                return length * 0.0003048
+            } else {
+                return length
+            }
+        case "yards":
+            if toUnit == "meters" {
+                return length * 0.9144
+            } else if toUnit == "miles" {
+                return length * 0.000568181818
+            } else if toUnit == "kilometers" {
+                return length * 0.0009144
+            } else if toUnit == "feet" {
+                return length * 3
+            } else {
+                return length
+            }
+        case "miles":
+            if toUnit == "yards" {
+                return length * 1760
+            } else if toUnit == "meters" {
+                return length * 1609.344
+            } else if toUnit == "kilometers" {
+                return length * 1.609344
+            } else if toUnit == "feet" {
+                return length * 5280
+            } else {
+                return length
+            }
+        default:
+            return length
+        }
+    }
     
     let lengthUnits = ["meters", "kilometers", "feet", "yards", "miles"]
     
@@ -55,6 +122,12 @@ struct ContentView: View {
                             Text("\($0)")
                         }
                     }
+                }
+                
+                Section {
+                    Text("\(result, specifier: "%.2f")")
+                } header: {
+                    Text("\(length) \(fromUnit) in \(toUnit) is")
                 }
             }.navigationTitle("Length conversion")
             
