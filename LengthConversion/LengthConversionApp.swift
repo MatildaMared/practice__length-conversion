07830cc473624e9ce8156344fe4231144eae7e24
Matilda Mared <matildamared@live.se>
2022-10-20 09:35:52 +0200

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Matilda Mared on 2022-10-20.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
